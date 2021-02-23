4e456e29cca0641d2226d9270bf5d69af2895814
zhaodong_ <464576716@qq.com>
2021-02-23 17:54:40 +0800

    init project

---
+++ website/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <button @click="add">count is: {{ count }}</button>
+</template>
+
+<script lang="ts">
+  import { ref, defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'HelloWorld',
+    props: {
+      msg: {
+        type: String,
+        required: true
+      }
+    },
+    setup: () => {
+      const count = ref(0)
+      const add = () => {
+        count.value++
+      }
+      return { count, add }
+    }
+  })
+</script>
+
+<style scoped>
+  a {
+    color: #42b983;
+  }
+
+  label {
+    margin: 0 0.5em;
+    font-weight: bold;
+  }
+
+  code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+  }
+</style>
