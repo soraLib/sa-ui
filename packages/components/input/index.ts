daf532f1ccba6f327758119a7c23357397ca158c
hfutsora <346762712@qq.com>
2022-09-22 16:18:32 +0800

    feat: package components install util

---
+++ packages/components/input/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from '@sa-ui/utils'
+import Input from './src/input.vue'
+
+export const SInput = withInstall(Input)
+
+export * from './src/input'
---
+++ packages/components/input/src/input.ts
@@ -0,0 +1,8 @@
+import type Input from './input.vue'
+
+export const inputProps = {
+  // TODO: build props util
+}
+
+export type InputProps = typeof inputProps
+export type InputInstance = InstanceType<typeof Input>
